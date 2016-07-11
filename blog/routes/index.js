43ff01e1e0c55deb8eb8b3d8840b1977632d372d
tangyubin <774267423@qq.com>
2016-07-11 13:15:41 +0800

    init project

--- blog/routes/index.js
+++ blog/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/nswbmw', function(req, res){
+	res.send('hello, world!');	
+});
+
 module.exports = router;
